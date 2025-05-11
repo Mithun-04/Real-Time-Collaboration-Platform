92e1fba230d957dc2f2b0b8b2a742e3f26c48081
Mithun-04 <mithunkarthik123@gmail.com>
2025-05-12 00:02:36 +0530

    home

---
+++ Frontend/app/providers.js
@@ -0,0 +1,12 @@
+// app/providers.tsx
+'use client'
+
+import { HeroUIProvider } from '@heroui/system';
+
+export function Providers({children}) {
+  return (
+    <HeroUIProvider>
+      {children}
+    </HeroUIProvider>
+  )
+}
